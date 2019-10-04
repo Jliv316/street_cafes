7166486f8fd870491d045f084a0a02b4b1359f44
Jliv316 <jclivingston316@gmail.com>
2019-10-04 15:25:03 -0600

    adds category column to restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_205328) do
+ActiveRecord::Schema.define(version: 2019_10_04_212414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_10_04_205328) do
     t.integer "number_of_chairs"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
 end
