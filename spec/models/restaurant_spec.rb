71fe4b28b55fd03c140093264c805108f139b730
Jliv316 <jclivingston316@gmail.com>
2019-10-07 17:09:16 -0600

    adds test for other category

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -16,6 +16,7 @@ describe Restaurant, type: :model do
         ls1_large = create(:restaurant, postal_code: "LS1 JCL", number_of_chairs: 120)
         ls2_small = create(:restaurant, postal_code: "LS2 JCL", number_of_chairs: 11)
         ls2_large = create(:restaurant, postal_code: "LS2 JCL", number_of_chairs: 200)
+        other = create(:restaurant, postal_code: "LS10 JCL", number_of_chairs: 51)
 
 
         expect(Restaurant.find_all_ls1_small.first.number_of_chairs).to eq(9)
@@ -23,18 +24,7 @@ describe Restaurant, type: :model do
         expect(Restaurant.find_all_ls1_large.first.number_of_chairs).to eq(120)
         expect(Restaurant.find_all_ls2_small.first.number_of_chairs).to eq(11)
         expect(Restaurant.find_all_ls2_large.first.number_of_chairs).to eq(200)
-      end
-    end
-
-    describe 'all ls2 records' do
-      it 'should grab the correct restaurant records based on number of chairs' do
-
-      end
-    end
-
-    describe 'all other records' do
-      it 'should grab the correct restaurant records based on number of chair' do
-
+        expect(Restaurant.find_all_other.first.number_of_chairs).to eq(51)
       end
     end
   end
